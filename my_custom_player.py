86594325181e0e67b251fa143c5a31b0ef7c6215
Eric <tang8156@gmail.com>
2020-05-06 10:22:47 -0400

    opening book works

--- my_custom_player.py
+++ my_custom_player.py
@@ -1,10 +1,51 @@
-
+from enum import IntEnum
 from sample_players import DataPlayer
+import pickle
 
+class OpeningBookConfig(IntEnum):
+    DISABLED = 0,
+    EVALUATION = 1,
+    TRAINING = 2
 
 class CustomPlayer(DataPlayer):
+    def writeFile(self, filename, history, data=None):
+        t = {}
+        for key in history:
+            statePlusMove = history[key]
+            t[statePlusMove["board"]] = statePlusMove["move"]
+        sizeBeforeMerge = len(data)
+        if(data != None):
+            t.update(data)
+        sizeAfterMerge = len(t)
+        if(sizeBeforeMerge == sizeAfterMerge):
+            return
+        with open(filename, "wb") as f:
+            pickle.dump(t, f)
+
+    def __del__(self):
+        self.filename = "data.pickle"
+        
+        if(self.openingBookConfig != OpeningBookConfig.TRAINING):
+            return
+        from os import path
+        exists = path.exists(self.filename)
+        if(exists == False):
+            return
+
+        history = self.context["history"]
+        if(history == None):
+            return
+
+        with open(self.filename, "rb") as f:
+            data = pickle.load(f)
+            self.writeFile(self.filename, history, data)
+
     def __init__(self, player_id):
         super().__init__(player_id)
+        # OpeningBookConfig.DISABLED: do not use apply opening book technique. Will not use values from "data.pickle".
+        # OpeningBookConfig.EVALUATION: uses "data.pickle" values for next actions. Does not write new moves to "data.pickle".
+        # OpeningBookConfig.TRAINING: uses "data.pickle" values for next actions. Does save new moves to "data.pickle" file.
+        self.openingBookConfig = OpeningBookConfig.TRAINING
 
         ranges = [(52, 58), (0, 6), (5, 11), (57, 63)]
         arrs = []
@@ -22,6 +63,16 @@ class CustomPlayer(DataPlayer):
         self.q2 = arrs[1]
         self.q3 = arrs[2]
         self.q4 = arrs[3]
+
+        self.context = {"history": {}}
+    
+    def insertHistory(self, state, bestMove):
+        if bestMove == None:
+            self.context["history"] = None
+        else:
+            history = self.context["history"]
+            if(history != None and len(history) < 4):
+                self.context["history"][state.ply_count] = {"board": state.board, "move": bestMove}
 
     """ Implement your own agent to play knight's Isolation
 
@@ -56,6 +107,14 @@ class CustomPlayer(DataPlayer):
           Refer to (and use!) the Isolation.play() function to run games.
         **********************************************************************
         """
+        if(self.openingBookConfig == OpeningBookConfig.EVALUATION):
+            book = self.data
+            if(book != None and state.ply_count >= 2 and state.ply_count <= 9):
+                if(state.board in book):
+                    act = book[state.board]
+                    self.queue.put(act)
+                    return
+
         if state.ply_count < 2:
             acts = state.actions()
             # Choose the middle action. If this is the first move, choose the center
@@ -69,10 +128,11 @@ class CustomPlayer(DataPlayer):
                 self.queue.put(act)
         else:
             for i in range(1, 5):
-                bestMove = self.alpha_beta_search(state, depth=i)
+                temp = bestMove = self.alpha_beta_search(state, depth=i)
                 if bestMove == None:
                     bestMove = state.actions()[0]
                 self.queue.put(bestMove)
+                self.insertHistory(state, temp)
 
     def alpha_beta_search(self, state, depth):
         """ Return the move along a branch of the game tree that
